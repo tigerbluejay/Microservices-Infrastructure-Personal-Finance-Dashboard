29dcc1e94dd050a65f06bfb73fcd8dec99622911
Jose Maria Iriarte <josemariairiarte@gmail.com>
2025-11-04 16:36:26 -0300

    MarketData.Service: Refactor Program.cs and add ApplicationServiceExtensions... within it add CustomExceptionHandling

---
+++ Portfolio-Insights/MarketData.Service/Extensions/ApplicationServiceExtensions.cs
@@ -0,0 +1,53 @@
+﻿using BuildingBlocks.Behaviors;
+using BuildingBlocks.Exceptions;
+using BuildingBlocks.Messaging.MassTransit;
+using Carter;
+using FluentValidation;
+using MarketData.Service.Data;
+using MarketData.Service.Events;
+using MarketData.Service.Repositories;
+using MarketData.Service.Services;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace MarketData.Service.Extensions
+{
+    public static class ApplicationServiceExtensions
+    {
+        public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            var assembly = typeof(ApplicationServiceExtensions).Assembly;
+
+            services.AddCarter();
+
+            services.AddMediatR(config =>
+            {
+                config.RegisterServicesFromAssembly(assembly);
+                config.AddOpenBehavior(typeof(ValidationBehavior<,>));
+                config.AddOpenBehavior(typeof(LoggingBehavior<,>));
+            });
+
+            services.AddValidatorsFromAssembly(assembly);
+            services.AddGrpc();
+            services.AddMessageBroker(configuration);
+
+            services.AddSingleton<IMarketPriceRepository, InMemoryMarketPriceRepository>();
+            services.AddScoped<IMarketPricesPublisher, MarketPricesPublisher>();
+            services.Decorate<IMarketPriceRepository, CachedMarketPriceRepository>();
+
+            services.AddDbContext<MarketDataContext>(options =>
+                options.UseSqlite(configuration.GetConnectionString("Database")));
+
+            services.AddStackExchangeRedisCache(options =>
+                options.Configuration = configuration.GetConnectionString("Redis"));
+
+            // Cross-cutting concerns
+            services.AddExceptionHandler<CustomExceptionHandler>();
+            services.AddHealthChecks()
+                .AddRedis(configuration.GetConnectionString("Redis")!);
+
+            return services;
+        }
+    }
+}
--- Portfolio-Insights/MarketData.Service/Program.cs
+++ Portfolio-Insights/MarketData.Service/Program.cs
@@ -3,46 +3,23 @@ using BuildingBlocks.Messaging.MassTransit;
 using Carter;
 using FluentValidation;
 using HealthChecks.UI.Client;
-using MarketData.Service.Data;
-using MarketData.Service.Events;
 using MarketData.Service.Extensions;
-using MarketData.Service.Repositories;
 using MarketData.Service.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Very verbose logging
+// Verbose logging
 builder.Logging.ClearProviders();
 builder.Logging.AddConsole();
 builder.Logging.SetMinimumLevel(LogLevel.Trace);
 
-// Add services
-builder.Services.AddCarter();
-var assembly = typeof(Program).Assembly;
-
-builder.Services.AddMediatR(config =>
-{
-    config.RegisterServicesFromAssembly(assembly);
-    config.AddOpenBehavior(typeof(ValidationBehavior<,>));
-    config.AddOpenBehavior(typeof(LoggingBehavior<,>));
-});
-builder.Services.AddValidatorsFromAssembly(assembly);
-builder.Services.AddGrpc();
-builder.Services.AddMessageBroker(builder.Configuration);
-builder.Services.AddSingleton<IMarketPriceRepository, InMemoryMarketPriceRepository>();
-builder.Services.AddScoped<IMarketPricesPublisher, MarketPricesPublisher>();
-builder.Services.Decorate<IMarketPriceRepository, CachedMarketPriceRepository>();
-builder.Services.AddDbContext<MarketDataContext>(options =>
-    options.UseSqlite(builder.Configuration.GetConnectionString("Database")));
-builder.Services.AddStackExchangeRedisCache(options =>
-    options.Configuration = builder.Configuration.GetConnectionString("Redis"));
-builder.Services.AddHealthChecks()
-    .AddRedis(builder.Configuration.GetConnectionString("Redis")!);
+// Add application services
+builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
 
-// Exception handling middleware for **detailed logging**
+// Exception handling middleware for detailed logging
 app.Use(async (context, next) =>
 {
     try
@@ -64,7 +41,7 @@ app.Use(async (context, next) =>
 });
 
 app.UseHttpsRedirection();
-app.UseMigration(); // ensure DB migrations are applied
+app.UseMigration();
 app.MapGrpcService<MarketDataGrpcService>();
 app.MapHealthChecks("/health", new Microsoft.AspNetCore.Diagnostics.HealthChecks.HealthCheckOptions
 {
